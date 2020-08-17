1d31a6bf32b4f33d2d91c82f9bc4e72501837581
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-17 15:49:52 -0700

    added head function

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const head = (arr) => {
+  return arr[0];
+};
+
+assertEqual(head([1, 2, 3, 4]), 1);
