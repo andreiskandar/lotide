1478c20a3b10d009a5f602c79c7034d8f666d0af
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-18 13:29:44 -0700

    added eqArray

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const assertEqual = (actual, expected) => {
+	if (actual === expected) {
+		console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+	}
+};
+
+const eqArrays = (arr1, arr2) => {
+	return arr1.every((el, index) => el === arr2[index]);
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
