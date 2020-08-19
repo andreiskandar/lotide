fdf39192215aea858e6e2f7d08f093dfadc2400c
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-19 12:15:15 -0700

    worked on middle.js and tested

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = (actual, expected) => {
+	if (actual === expected) {
+		console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+	}
+};
+
+const bestTVShowsByGenre = {
+	sci_fi: 'The Expanse',
+	comedy: 'Brooklyn Nine-Nine',
+	drama: 'The Wire',
+};
+
+const findKeyByValue = (obj, titleName) => {
+	// Object.keys & Object.values
+	const keys = Object.keys(obj),
+		titles = Object.values(obj);
+
+	//using for loop
+	// for (let i = 0; i < titles.length; i++) {
+	//   if (titles[i] === titleName) {
+	//     return genre[i];
+	//   }
+	// }
+
+	//using filter
+	return keys.filter((genre) => obj[genre] === titleName);
+
+	// for (const genre in obj) {
+	// 	if (obj[genre] === title) return genre;
+	// }
+};
+console.log(findKeyByValue(bestTVShowsByGenre, 'The Wire'));
+// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- middle.js
+++ middle.js
@@ -9,20 +9,24 @@ const assertArraysEqual = (arr1, arr2) => {
 		: console.log(`🛑🛑🛑 Assertion Failed: "${arr1}" !== "${arr2}"`);
 };
 
-const middle = (arr) => {
+const middle = arr => {
 	return arr.length === 1 || arr.length === 2
 		? []
 		: isOdd(arr)
-		? arr[Math.floor(arr.length / 2)]
+		? [arr[Math.floor(arr.length / 2)]]
 		: [arr[arr.length / 2 - 1], arr[arr.length / 2]];
 };
 
-const isOdd = (arr) => {
+const isOdd = arr => {
 	return arr.length % 2 === 1;
 };
 
-console.log(middle([1, 2, 3, 4, 5]));
-const array = [1, 'two', { a: 1 }, 4];
-const mid = [...middle(array)];
-console.log(mid);
-assertArraysEqual(mid, ['two', { a: 1 }]);
+// console.log(middle([1, 2, 3, 4, 5]));
+const array = [1, 'two', 4];
+
+const mid = middle(array);
+
+//question: ['t','w','o'] and ['two'] assertion is equal???
+assertArraysEqual(mid, ['two']);
+// assertArraysEqual(midInArray, ['two']);
+// assertArraysEqual(mid, ['two', { a: 1 }]);
